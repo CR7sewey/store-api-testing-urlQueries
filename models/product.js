0deb1473b3010a6eef55396bbe6ae87dbb842f82
CR7sewey <miguelmotacarvalho@gmail.com>
2024-04-18 18:01:05 +0100

    Model

---
+++ models/product.js
@@ -0,0 +1,36 @@
+const moongose = require("mongoose");
+
+const ProductSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "must provide name"],
+    trim: true,
+    maxlength: [20, "name can not be more than 20 characters"],
+  },
+  price: {
+    type: Number,
+    required: [true, "must provide a price"],
+  },
+  featured: {
+    type: Boolean,
+    default: false,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  company: {
+    type: String,
+    required: [true, "must provide company"],
+    enum: {
+      values: ["ikea", "liddy", "caressa", "marcos"],
+      message: "{VALUE} is not supported",
+    },
+  },
+});
+
+module.exports = mongoose.model("Product", ProductSchema);
